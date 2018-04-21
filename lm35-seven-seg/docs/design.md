804c83cca48716a93074b9588c179746306b7e35
john wiltse <speed99mhz@yahoo.com>
2018-04-20 19:43:35 -0700

    documentation

--- lm35-seven-seg/docs/design.md
+++ lm35-seven-seg/docs/design.md
@@ -1,56 +1,55 @@
-Assumptions:
-- Temp will be in degree C
-- 2 digits to right, and 2 digits to left of decimal point. Example 21.28 deg C
-- Decimal point will be fixed at second digit.
-- Union 8 IO lines to a logical PORT so we can just latch the port like: DISPLAY_PORT = digitValue;
-- Segment display PORT ‘a’ = LSD ‘dp’ = MSD
-- Allow users to adjust the timer to slow down and see the multiplexing
-- Watch over driving the LEDs for when its slowed aim for ~20mA
-- Digits will multiplex on timer1 start out with ~75Hz or change digits every third interrupt.
-- 8 Pins for DISPLAY_PORT PD0-7 (Arduino pins 0 - 7)
-- 4 Pins for digits PB0-3 (Arduino pins 8 - 11)
-- Will write directly to AVR ports and not use digitalWrite to make this easy, clean and quick
-- 1 analog pin for LM35 (Arduino pin A0)
+# Project Planning Notes and Pseudo Code
+### Assumptions:
+1. Temp will be in degree C
+2. 2 digits to right, and 2 digits to left of decimal point. Example 21.28 deg C
+3. Decimal point will be fixed at second digit.
+4. Union 8 IO lines to a logical PORT so we can just latch the port like: DISPLAY_PORT = digitValue; *Not implemented*
+5. Segment display PORT ‘a’ = LSD ‘dp’ = MSD
+6. Allow users to adjust the timer to slow down and see the multiplexing
+7. Watch over driving the LEDs for when its slowed aim for ~20mA
+8. Digits will multiplex on timer1 start out with ~75Hz or change digits every third interrupt.
+9. 8 Pins for DISPLAY_PORT PD0-7 (Arduino pins 0 - 7)
+10. 4 Pins for digits PB0-3 (Arduino pins 8 - 11)
+11. Will write directly to AVR ports and not use digitalWrite to make this easy, clean and quick
+12. 1 analog pin for LM35 (Arduino pin A0)
 
-Basic Workflow
+### Calculations
 
-#define REFRESH_DIVIDE 3
+We need a mapping between an integer variable literal 1 to displaying it with the seven segment display. Using the following array
+```C
+char array digitMap = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7C, 0x07, 0x7F, 0x67]
+```
+#### Value calculations
 
-char[] digitMap = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7C, 0x07, 0x7F, 0x67]
-// zero   = segments a, b, c, d, e, f         = 0011:1111 = 0x3F
-// one    = segments b + c                    = 0000:0110 = 0x06
-// two    = segments a + b + d + e + g        = 0101:1011 = 0x5B
-// three  = segments a + b + c + d + g        = 0100:1111 = 0x4F
-// four   = segments b + c + f + g            = 0110:0110 = 0x66
-// five   = segments a + c + d + f + g        = 0110:1101 = 0x6D
-// six    = segments c + d + e + f + g        = 0111:1100 = 0x7C
-// seven  = segments a + b + c                = 0000:0111 = 0x07
-// eight  = segments a + b + c + d + e +f + g = 0111:1111 = 0x7F
-// nine   = segments a + b + c + f + g        = 0110:0111 = 0x67
+digit | segments lit | BIN Value | HEX value
+------|--------------|-----------|----------
+zero | a, b, c, d, e, f | 0011:1111 | 0x3F
+one | b + c | 0000:0110 | 0x06
+two | a + b + d + e + g | 0101:1011 | 0x5B
+three | a + b + c + d + g | 0100:1111 | 0x4F
+four | b + c + f + g | 0110:0110 | 0x66
+five | a + c + d + f + g | 0110:1101 | 0x6D
+six | c + d + e + f + g | 0111:1100 | 0x7C
+seven | a + b + c | 0000:0111 | 0x07
+eight | a + b + c + d + e +f + g | 0111:1111 | 0x7F
+nine | a + b + c + f + g | 0110:0111 | 0x67
 
-char segmentNumbers[] = [2, 1, 3, 4]
-char decimalCharacter = 0x80 // segment h = 1000:0000
-int activeSegment = 0 to 3
-volatile Bool switchSegment = false;  //used to signal a digit change
-volatile bool readADC = false
+### Pseudo Code
 
-
-On MsTimer2 interrupt:
-//only change volatile variables, don’t set any pins/ports, delay too long etc.
-    - Change the active segment to the next one
-    - switchSegment = true //flag for main loop to change segments. We want this ISR AFAP
-
-On timer1 interrupt:
-https://playground.arduino.cc/Code/Timer1
+On MsTimer2 interrupt:<br />
+*only change volatile variables, don’t set any pins/ports, delay too long etc.*
+- Change the active segment to the next one
+- switchSegment = true //flag for main loop to change segments. We want this ISR AFAP
 - readADC = true
+https://playground.arduino.cc/Code/Timer1
 
-SplitDigits:
+SplitDigits Method:
 - mask off first digit from input and store in segmentNumbers[3]
 - mask off second digit from input and store in segmentNumbers[2]
 - mask off third digit from input and store in segmentNumbers[1]
 - mask off forth digit from input and store in segmentNumbers[0]
 
-getTemp:
+getTemp Method:
 - read the ADC
 - Calculate the Temp
 - Call split up the digits with the temperature value
--- lm35-seven-seg/src/main.cpp
+++ lm35-seven-seg/src/main.cpp
@@ -1,37 +1,71 @@
+/* main.cpp
+ * Entry point for 4 digit seven segment display example for ACMW Bellevue
+ * friday talk.
+ */
+
+/* Helpful libraries
+ */
 #include <Arduino.h>
 #include <MsTimer2.h>
 
+/* Application constants
+ */
 #define TEMPERATURE_READ_WAITS 250
 #define TIMER_TWO_MS_TIMEOUT 4
 #define SERIAL_SPEED 115200
+#define DECIMAL_BIT 7
 
-volatile char segmentNumbers[5] =
-    {2, 5, 0, 0, 0}; //buffer for current numbers to display
+/* Variables Needed
+ */
+volatile bool switchSegment = false;    // used to signal a digit change
+uint8_t segmentNumbers[5] =
+    {2, 5, 0, 0, 0};                    //buffer for current numbers to display
 uint8_t digitMap[] =
     {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7C, 0x07, 0x7F, 0x67};
-uint8_t decimalBit = 7;                     // segment h = 1000:0000
-float temp = 0.0;
-uint8_t output = 1;
-volatile uint8_t activeSegment = 1;         // 1 - 4
-volatile bool switchSegment = false;        // used to signal a digit change
-volatile bool readADC = false;              // should we get a new reading
+uint8_t segmentIndex = 0;
+uint8_t currentDigit = 0;
+volatile uint8_t activeSegment = 1;     // 1 - 4
 uint16_t interruptWaits = TEMPERATURE_READ_WAITS;
-uint8_t staticTestDigit = 0;
+float temperature = 0.0;
 
-/*
-    ISR nextSegment
-    bit shit segment to the left, or wrap right. signal to switch segments
+/* function prototypes
  */
-void nextSegment()
-{
-    activeSegment <<= 1;
-    if (activeSegment > 0b00001000)
-        activeSegment = 0b00000001;
-    switchSegment = true; //flag for main loop to change segments
+void setup();
+void nextSegment();
+void setDisplayBuffer(int number, int multiplier, int placement);
+void setPorts(uint8_t segment);
+
+/* loop
+ * Main loop of the application. runs forever
+ * Checks if the segment needs to be changed, and if we need to get another
+ * tempeture reading. We only do this every so often to not spend too much
+ * time reading the ADC.
+ */
+void loop() {
+
+    if (switchSegment)
+    {
+        if (--interruptWaits == 0)
+        {
+            temperature = (analogRead(A1) * 5.0 / 1024.0);
+                                // Read analog pin
+                                // mV on A1 is a ratio of the full resolution of the ADC @ 5V
+            temperature = temperature / 0.01; // we are left with .01 V/deg C
+
+            interruptWaits = TEMPERATURE_READ_WAITS; //reset our counter to delay again
+            //Serial.println((int)(temperature*100));
+            setDisplayBuffer((int)(temperature*100), 1000, 0);
+        }
+        setPorts(activeSegment);
+        switchSegment = false;
+    }
 }
 
-/*
-    Set up our application.
+/* setup
+ * Set up our application.
+ * Define the serial connection for debugging
+ * Set up timer 2 for segment change
+ * set all digital ports to outputs
  */
 void setup() {
     Serial.begin(SERIAL_SPEED);
@@ -45,9 +79,21 @@ void setup() {
     }
 }
 
-/*
-    setDisplayBuffer
-    recursive function to save off the individule digits in a integer
+/* nextSegment ISR
+ * bit shit segment to the left, or wrap right. signal to switch segments
+ */
+void nextSegment()
+{
+    activeSegment <<= 1;
+    if (activeSegment > 0b00001000)
+        activeSegment = 0b00000001;
+    switchSegment = true; //flag for main loop to change segments
+}
+
+/* setDisplayBuffer
+ * Recursive function to save off the individule digits in a integer
+ * Example 21.45 would leave segmentNumbers as [2, 1, 4, 5]
+ * Similar to sprintf without leaving the array values in ascii
  */
 void setDisplayBuffer(int number, int multiplier, int placement)
 {
@@ -58,56 +104,37 @@ void setDisplayBuffer(int number, int multiplier, int placement)
         setDisplayBuffer(remainingNumber, multiplier/10, ++placement);
 }
 
-/*
-    setPorts
-    outputs portD and portB
+/* setPorts
+ * sets the individual pins on portB and portD with the value of digit
+ * and latches (sync low) the segment
+ * Example if digit is 0x5B (2) and segment is 3 then port b and d are set:
+ * -Digit Latches- ---------Segment Latches--------- -unused
+ * PB5 PB4 PB3 PB2 PB1 PB0 : PD7 PD6 PD5 PD4 PD3 PD2 PD1 PD0
+ *  1   1   0   1   0   1     0   1   1   0   1   1   x   x
  */
-void setPorts(uint8_t digit, uint8_t segment)
+void setPorts(uint8_t segment)
 {
-    int index = log(segment)/log(2);
+    segmentIndex = log(segment)/log(2);
     // ^^^ same same
     //if (segment == 8) index = 3;
     //if (segment == 4) index = 2;
     //if (segment == 2) index = 1;
     //if (segment == 1) index = 0;
 
-    digit = digitMap[segmentNumbers[index]];
-    PORTD = digit << 2;
+    currentDigit = digitMap[segmentNumbers[segmentIndex]]; //map ex. 2 to 0x5B
+    PORTD = currentDigit << 2; //shift 2 left because PD0/1 are RDX/TXD
     PORTB = 0;
 
-    if (index == 1)
-        digit |= (1 << decimalBit); //hard coded decimal point to second digit
+    /* TODO:
+     * digit scan is left to right. (MSD --> LSD)
+     * probably better to scan left to right (MSD <-- LSD) for consistancy
+     * in that case decimal point will be at index 2 not 1.
+     */
+    if (segmentIndex == 1)
+        currentDigit |= (1 << DECIMAL_BIT); //hard coded decimal point
 
-    PORTB = ((digit >> 6) | (segment << 2));
+    PORTB = ((currentDigit >> 6) | (segment << 2));
 
     // PORTB looks like this xx01:00xx and we want this xx10:11xx
-    PORTB ^= (1 << 2);
-    PORTB ^= (1 << 3);
-    PORTB ^= (1 << 4);
-    PORTB ^= (1 << 5);
-}
-
-/*
-    main loop of the application. runs forever
- */
-void loop() {
-
-    if (switchSegment)
-    {
-        if (--interruptWaits == 0)
-        {
-            temp = (analogRead(A1) * 5.0 / 1024.0);
-                                // Read analog pin
-                                // mV on A1 is a ratio of the full resolution of the ADC @ 5V
-            temp = temp / 0.01; // we are left with .01 V/deg C
-
-            interruptWaits = TEMPERATURE_READ_WAITS; //reset our counter to delay again
-            Serial.println((int)(temp*100));
-            for(int i=0; i<5; i++)
-                segmentNumbers[i] = 0;
-            setDisplayBuffer((int)(temp*100), 1000, 0);
-        }
-        setPorts(digitMap[staticTestDigit], activeSegment);
-        switchSegment = false;
-    }
+    PORTB ^= 0x3C; // reverse bits 2:5 xx111100 = 0x3C
 }
